794d3ccb80160f21f11fb9c4a3564f93359072ea
Chhai Chivon <chivonchhai.it@gmail.com>
2023-05-09 16:42:32 +0700

    Add crud of shared_preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,103 +14,158 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+  final TextEditingController _textEditingController = TextEditingController();
+  late SharedPreferences _prefs;
+  List<String> _items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    _prefs = await SharedPreferences.getInstance();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _items = _prefs.getStringList('items') ?? [];
     });
+  }
+
+  Future<void> _addItem(String item) async {
+    setState(() {
+      _items.add(item);
+    });
+    await _prefs.setStringList('items', _items);
+  }
+
+  Future<void> _updateItem(int index, String newItem) async {
+    setState(() {
+      _items[index] = newItem;
+    });
+    await _prefs.setStringList('items', _items);
+  }
+
+  Future<void> _deleteItem(int index) async {
+    setState(() {
+      _items.removeAt(index);
+    });
+    await _prefs.setStringList('items', _items);
+  }
+
+  Future<void> _clearData() async {
+    setState(() {
+      _items.clear();
+    });
+    await _prefs.remove('items');
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Flutter SharedPreferences Demo'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          TextField(
+            controller: _textEditingController,
+            decoration: InputDecoration(
+              hintText: 'Enter a new item',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              await _addItem(_textEditingController.text);
+              _textEditingController.clear();
+            },
+            child: Text('Add'),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _items.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Dismissible(
+                  key: ValueKey(_items[index]),
+                  onDismissed: (direction) async {
+                    if (direction == DismissDirection.endToStart) {
+                      await _deleteItem(index);
+                    } else if (direction == DismissDirection.startToEnd) {
+                      // Show dialog to edit item
+                      String? newItem = await showDialog<String>(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Edit Item'),
+                            content: TextField(
+                              controller: TextEditingController(text: _items[index]),
+                              autofocus: true,
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text('Cancel'),
+                              ),
+                              TextButton(
+                                onPressed: () async {
+                                  await _updateItem(index, _textEditingController.text);
+                                  Navigator.of(context).pop(_textEditingController.text);
+                                },
+                                child: Text('Save'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  },
+                  background: Container(
+                    color: Colors.green,
+                    alignment: Alignment.centerLeft,
+                    child: Icon(Icons.edit),
+                  ),
+                  secondaryBackground: Container(
+                    color: Colors.red,
+                    alignment: Alignment.centerRight,
+                    child: Icon(Icons.delete),
+                  ),
+                  child: Card(
+                    elevation: 3,
+                    child: ListTile(
+                      title: Text(_items[index]),
+                    ),
+                  ),
+                );
+              },
             ),
-          ],
-        ),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              await _clearData();
+            },
+            child: Text('Clear All'),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
